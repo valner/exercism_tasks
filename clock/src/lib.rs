ad392dec9a968d33aef5d700099e97143df53703
valner <valner47@gmail.com>
2022-08-07 12:08:01 +0300

    clock solved

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,54 @@
-pub struct Clock;
+use core::fmt;
+#[derive(PartialEq, Eq)]
+pub struct Clock {
+    hours : i32,
+    minutes : i32
+}
+
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock{hours, minutes}.normalize()
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+    pub fn add_minutes(mut self, minutes: i32) -> Self {
+        self.minutes += minutes;
+        self.normalize()
+
+    }
+
+    pub fn to_string(self) -> String {
+        format!("{:02}:{:02}", self.hours, self.minutes)
+    }
+
+    fn normalize(mut self) -> Self{
+        let mut carry_hours: i32 = 0;
+        if self.minutes.abs() > 59 {
+            carry_hours = self.minutes.signum();
+            carry_hours *= self.minutes.abs() / 60;
+            self.minutes = self.minutes.signum() * (self.minutes.abs() % 60 );
+        }
+
+        if self.minutes < 0 {
+            self.minutes = 60 + self.minutes;
+            carry_hours -= 1;
+        }
+
+        self.hours += carry_hours;
+
+        self.hours = self.hours.signum() * (self.hours.abs() % 24);
+
+        if self.hours < 0 {
+            self.hours = 24 + self.hours;
+        }
+
+        self
+    }
+}
+
+
+impl fmt::Debug for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
